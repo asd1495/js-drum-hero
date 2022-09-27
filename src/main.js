a61d4ad98f59a7d55c57352f0d95446b998bcca5
asd1495 <armando.salcedo95@gmail.com>
2022-09-26 19:46:07 -0500

    added the script for the drums to work, but not working on local server :( , ill fix it soon

--- src/main.js
+++ src/main.js
@@ -9,8 +9,27 @@ window.addEventListener("load", function() {
     )
 });
 
-document.querySelector(".popup").addEventListener("click", function() {
+document.querySelector("#close").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "none";
 });
 
-//Drumkit
+//Drumkit 
+function sound(e) {
+    const audio = this.document.querySelector(`audio[data-key=${e.code}]`);
+    const key = this.document.querySelector(`.key[data-key=${e.code}]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    key.classList.add("playing");
+}
+
+function rmvTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", rmvTransition));
+
+window.addEventListener("keydown", sound);
